31ad1252f19cfdf12f2010043a653908070e4335
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-06-11 00:04:03 -0300

    test: ensure Invoices is correct instantiated

---
+++ src/domain/entities/invoices.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest'
+import { Invoices } from './Invoices'
+import { makeInvoices } from '@/src/tests/factories/invoices'
+
+describe('Invoices entity', () => {
+  it('Should be able to instance an Invoices with correct values', () => {
+    const sut = makeInvoices()
+
+    expect(sut).toBeInstanceOf(Invoices)
+    expect(sut).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        month: expect.any(Number),
+        amount: expect.any(Number),
+        isPaid: expect.any(Boolean),
+        residentId: expect.any(String),
+      }),
+    )
+  })
+})
