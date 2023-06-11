a36ad9ea55729e76996b8745180c6d5dadec552b
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-06-11 16:05:12 -0300

    test: ensure that buildingNumber returns a building on success

--- src/app/modules/building/add-building-use-case.spec.ts
+++ src/app/modules/building/add-building-use-case.spec.ts
@@ -1,8 +1,9 @@
-import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { AddBuilding } from './add-building-use-case'
 import { InMemoryBuildingRepository } from '@/src/infra/in-memory/building-repository'
 import { makeBuilding } from '@/src/tests/factories/entities'
 import { AppError } from '@/src/errors/global-error'
+import { Building } from '@/src/domain/entities/building'
 
 let sut: AddBuilding
 let inMemoryBuildingRepository: InMemoryBuildingRepository
@@ -13,7 +14,7 @@ describe('Auth Syndicate Use Case', () => {
     sut = new AddBuilding(inMemoryBuildingRepository)
   })
 
-  it('Shoudl throws if buildingNumber is already registered', () => {
+  it('Should throws if buildingNumber is already registered', () => {
     expect(async () => {
       const building = makeBuilding()
       inMemoryBuildingRepository.add(building)
@@ -21,5 +22,15 @@ describe('Auth Syndicate Use Case', () => {
         buildingNumber: building.buildingNumber,
       })
     }).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('Should return a building on success', async () => {
+    const { buildingNumber } = makeBuilding()
+    const { building } = await sut.execute({
+      buildingNumber,
+    })
+
+    expect(building).toBeInstanceOf(Building)
+    expect(building).toHaveProperty('id')
   })
 })
