1f5eb4430104caf431ce2bee42d76afbe355f68a
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-06-11 14:27:25 -0300

    test: ensure that Hash repository is beeing called with correct values

---
+++ src/app/modules/syndicate/auth.syndicate.spec.ts
@@ -0,0 +1,35 @@
+import { InMemorySyndicateRepository } from '@/src/infra/in-memory/syndicate-repository'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { AuthSyndicate } from './auth-syndicate'
+import { Auth, Hasher } from '@/src/domain/protocols'
+import { makeSyndicate } from '@/src/tests/factories/entities/syndicate'
+import { makeHashStub, makeAuthStub } from '@/src/tests/factories/infra'
+
+let sut: AuthSyndicate
+let inMemorySyndicateRepository: InMemorySyndicateRepository
+let authStub: Auth
+let hasherStub: Hasher
+
+describe('Auth Syndicate Use Case', () => {
+  beforeEach(() => {
+    inMemorySyndicateRepository = new InMemorySyndicateRepository()
+    hasherStub = makeHashStub()
+    authStub = makeAuthStub()
+    sut = new AuthSyndicate(inMemorySyndicateRepository, hasherStub, authStub)
+  })
+
+  it('Should call Hasher Repository with correct values', async () => {
+    const compareSpy = vi.spyOn(hasherStub, 'compare')
+
+    const syndicate = makeSyndicate()
+    const { username, password } = syndicate
+
+    inMemorySyndicateRepository.Syndicates.push(syndicate)
+    await sut.execute({
+      username,
+      password,
+    })
+
+    expect(compareSpy).toBeCalledWith(password, password)
+  })
+})
